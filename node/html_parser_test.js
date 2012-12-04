30dd43292b19e989a17702000cd2f173005a5501
diego <diego@diego.(none)>
2012-12-04 10:39:05 -0600

    Changed the functionality of the html parser so that it can be called from another file.

--- node/html_parser_test.js
+++ node/html_parser_test.js
@@ -1,22 +1,30 @@
 //Import required modules.
 var htmlparser = require('htmlparser'),
-           sys = require('sys'),
         select = require('soupselect').select,
            URI = require('URIjs'),
+            fs = require('fs'),
        handler = new htmlparser.DefaultHandler(get_links), //Define the handler,
         parser = new htmlparser.Parser(handler); //and the parser.
+
+function Node_parser(name){
+    this.name = name;
+    console.log("Hello" + name + ", my Node_parser is online now.");
+}
 
 //Gets the filename (e.g. foo.html) for all the links(<a> tags) in a html file.
 function get_links(error,dom){
     if(error)
 	console.log(error);
     else{
+	console.log("parsing started");
+	console.log(dom);
 	var a_tags = select(dom,'a');
 	a_tags.forEach(get_filename);
     }
 }
 
-//Returns the filenames (e.g. foo.html) for an array of links. Must be the callback for an array.forEach(callback).
+//Returns the filenames (e.g. foo.html) for an array of links. 
+//Meant to be called back from the get_links function.
 function get_filename(a_tag){
     var currentTag = new URI(a_tag.attribs.href);
     var filename = currentTag.filename();
@@ -24,6 +32,18 @@ function get_filename(a_tag){
     return filename
 }
 
-var rawHtml = "<html><head>You are what you is</head><body><a href='http://www.google.com/index.html#algo'>blabla</a><a href='http://www.yahoo.com/mail.html'>yahoo</a><a href='http://foo.com/bar.html#wtf'</a></body></html>";
-parser.parseComplete(rawHtml);
+function load_file(file){
+    try{
+        var file_data = fs.readFileSync(file);
+        console.log(file + " loaded");
+        console.log("file data:\n" + file_data);
+        return file_data;
+    }catch(err){
+        console.log(file + " not loaded: " + err);
+        throw err; //TODO this statement may need changing for release
+    }
+}
 
+var rawHtml = "<html><head>You are what you is</head><body><a href='http://www.google.com/index.html#algo'>blabla</a><a href='http://www.yahoo.com/mail.html'>yahoo</a><a href='http://foo.com/bar.html#wtf'</a></body></html>";
+//parser.parseComplete(load_file("index.html"));
+Node_parser.prototype.load_file = load_file;
